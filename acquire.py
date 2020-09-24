b3910a4a6aefc732b3643899489863edb7cc90ab
datastraine <anthony.straine@gmail.com>
2020-09-24 12:00:31 -0500

    updated acquire file and adds wrangle notebook

--- acquire.py
+++ acquire.py
@@ -1,25 +1,91 @@
 import pandas as pd
-import env
+import numpy as np
 import os
+from env import host, user, password
 
-def get_connection(db, user=env.user, host=env.host, password=env.password):
+#################### Acquire Mall Customers Data ##################
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def get_telco():
-    filename = "telco.csv"
+def new_mall_data():
+    '''
+    This function reads the mall customer data from the Codeup db into a df,
+    write it to a csv file, and returns the df. 
+    '''
+    sql_query = 'SELECT * FROM customers'
+    df = pd.read_sql(sql_query, get_connection('mall_customers'))
+    df.to_csv('mall_customers_df.csv')
+    return df
 
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
+def get_mall_data(cached=False):
+    '''
+    This function reads in mall customer data from Codeup database if cached == False 
+    or if cached == True reads in mall customer df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('mall_customers_df.csv') == False:
+        df = new_mall_data()
     else:
-        # read the SQL query into a dataframe
-        df = pd.read_sql("""select * from telco_churn.customers left join telco_churn.internet_service_types
-        on telco_churn.internet_service_types.internet_service_type_id = telco_churn.customers.internet_service_type_id left join telco_churn.payment_types
-on telco_churn.payment_types.payment_type_id = telco_churn.customers.payment_type_id left join telco_churn.contract_types
-on telco_churn.contract_types.contract_type_id = telco_churn.customers.contract_type_id
-""", get_connection('telco_churn'))
+        df = pd.read_csv('mall_customers_df.csv', index_col=0)
+    return df
 
-        # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_csv(filename, index=False)
+###################### Acquire Titanic Data ######################
 
-        # Return the dataframe to the calling code
-        return df 
+    
+def new_titanic_data():
+    '''
+    This function reads the titanic data from the Codeup db into a df,
+    write it to a csv file, and returns the df.
+    '''
+    sql_query = 'SELECT * FROM passengers'
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    df.to_csv('titanic_df.csv')
+    return df
+
+def get_titanic_data(cached=False):
+    '''
+    This function reads in titanic data from Codeup database if cached == False 
+    or if cached == True reads in titanic df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('titanic_df.csv') == False:
+        df = new_titanic_data()
+    else:
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+    return df
+
+###################### Acquire Iris Data ######################
+
+def new_iris_data():
+    '''
+    This function reads the iris data from the Codeup db into a df,
+    writes it to a csv file, and returns the df.
+    '''
+    sql_query = """
+                SELECT species_id,
+                species_name,
+                sepal_length,
+                sepal_width,
+                petal_length,
+                petal_width
+                FROM measurements
+                JOIN species
+                USING(species_id)
+                """
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    df.to_csv('iris_df.csv')
+    return df
+
+def get_iris_data(cached=False):
+    '''
+    This function reads in iris data from Codeup database if cached == False
+    or if cached == True reads in iris df from a csv file, returns df
+    '''
+    if cached or os.path.isfile('iris_df.csv') == False:
+        df = new_iris_data()
+    else:
+        df = pd.read_csv('iris_df.csv', index_col=0)
+    return df
