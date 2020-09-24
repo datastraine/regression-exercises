6cc2ece64da12bd3be32e14df0d40c988f23c493
datastraine <anthony.straine@gmail.com>
2020-09-24 16:28:57 -0500

    updates acquire.py and wrangle notebook and adds wrangle.py file

--- acquire.py
+++ acquire.py
@@ -12,6 +12,54 @@ def get_connection(db, user=user, host=host, password=password):
     '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
+def get_telco():
+    '''
+    This function reads all the Telco data from the CodeUp DB into a DF.
+    If telco.csv is not present on the local system it will conduct a DB pull, reads the pull into a DF, 
+    and saves the pull as a CSV.
+    If telco.csv is present on the local system it will read the csv into a DF
+    '''
+    filename = "telco.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql("""select * from telco_churn.customers left join telco_churn.internet_service_types
+        on telco_churn.internet_service_types.internet_service_type_id = telco_churn.customers.internet_service_type_id left join telco_churn.payment_types
+        on telco_churn.payment_types.payment_type_id = telco_churn.customers.payment_type_id left join telco_churn.contract_types
+        on telco_churn.contract_types.contract_type_id = telco_churn.customers.contract_type_id
+        """, get_connection('telco_churn'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename, index=False)
+
+        # Return the dataframe to the calling code
+        return df 
+
+def telco_two_year_charges():
+    '''
+    This function pulls customer_id, monthly_charages, total_charges, and tenure from CodeUp telco DB 
+    where contract_type_id = 3 (two year contracts)
+    If telco_two_year_charges.csv is not present on the local system it will conduct a DB pull, 
+    reads the pull into a DF, and saves the pull as a CSV.
+    If telco.csv is present on the local system it will read the csv into a DF
+    '''
+    filename = "telco_two_year_charges.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql("""select customer_id, monthly_charges, tenure, total_charges from telco_churn.customers
+        where contract_type_id = 3""", get_connection('telco_churn'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename, index=False)
+
+        # Return the dataframe to the calling code
+        return df 
+
 def new_mall_data():
     '''
     This function reads the mall customer data from the Codeup db into a df,
---
+++ wrangle.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import os
+import acquire
+
+def wrangle_telco():
+    df = acquire.telco_two_year_charges()
+    df['total_charges'] = pd.to_numeric(df['total_charges'],errors='coerce')
+    df.dropna(inplace = True) 
+    df['outlier_tenure'] = df['tenure'] < 13.5
+    return df
